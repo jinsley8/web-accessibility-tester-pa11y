59fe5fe1edc80b1d6347f95a99f2f1d447a125b9
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2021-12-18 16:26:09 -0500

    feat: Output a11y issues as cards

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ const testAccessibility = async (e) => {
       alert('Something went wrong');
     } else {
       const { issues } = await response.json();
-      
+
       addIssuesToDOM(issues);
       setLoading(false);
     }
@@ -25,7 +25,32 @@ const testAccessibility = async (e) => {
 
 // Add issues to DOM
 const addIssuesToDOM = (issues) => {
-  console.log(issues);
+  const issuesOutput = document.querySelector('#issues');
+
+  issuesOutput.innerHTML = '';
+
+  if(issues.length === 0) {
+    issuesOutput.innerHTML = '<h3>No Issues Found</h3>'
+  } else {
+    issues.forEach((issue) => {
+      const output = `
+        <div class="card mb-5">
+          <div class="card-body">
+            <h3>${issue.message}</h3>
+            <p class="bg-light p-3 my-3">
+              ${escapeHTML(issue.context)}
+            </p>
+
+            <p class="bg-secondary text-light p-2">
+              CODE: ${issue.code}
+            </p>
+          </div>
+        </div>
+      `
+
+      issuesOutput.innerHTML += output;
+    })
+  }
 }
 
 // Set loading state
@@ -40,6 +65,13 @@ const setLoading = (isLoading = true) => {
 }
 
 // Escape HTML
-
+function escapeHTML(html) {
+  return html
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
 
 document.querySelector('#form').addEventListener('submit', testAccessibility);
