951c1b5e72cf4a775a9fa2ff1d605c05ff37e607
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2021-12-19 13:59:45 -0500

    fix: Change start script as Heroku cannot run devDep/Nodemon

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ app.get('/api/test', async (req, res) => {
 // Set static folder
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
