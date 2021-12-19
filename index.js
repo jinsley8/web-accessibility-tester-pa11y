13b951f546ff5b0bdc2aaf640289ccb10714a041
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2021-12-19 16:14:10 -0500

    fix: Launch puppeteer in --no-sandbox to work on Heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import pa11y from 'pa11y';
 import cors from 'cors';
 import morgan from 'morgan';
 import chalk from 'chalk';
+import puppeteer from 'puppeteer';
 
 const app = express();
 
@@ -13,16 +14,35 @@ if (process.env.NODE_ENV === "development") {
 app.use(cors());
 
 app.get('/api/test', async (req, res) => {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+
   try {
     if(!req.query.url) {
       return res.status(400).json({error: 'URL is required'});
       // return res.status(400).send('URL is required');
     }
 
-    const results = await pa11y(req.query.url);
+    const results = await pa11y(req.query.url, {
+      browser: browser,
+      page,
+    });
     
     res.status(200).json(results);
+
+    // Close the browser instance and pages now we're done with it
+	  await page.close();
+		await browser.close();
+
   } catch (error) {
+    // Close the browser instance and pages if theys exist
+		if (page) {
+			await page.close();
+		}
+		if (browser) {
+			await browser.close();
+		}
+
     return res.status(500).json({error: error.message});
     // res.status(500).send('Server Error');
   }
